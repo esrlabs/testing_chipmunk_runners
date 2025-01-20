a59b6a9848545fbf096ed3349de142614cdfc444
itsmesamster <itsmesamster@gmail.com>
2025-01-20 11:53:28 +0100

    Update process.rb

--- scripts/benchmarks/process.rb
+++ scripts/benchmarks/process.rb
@@ -124,6 +124,7 @@ end
 
 def read_benchmark_data(file_path)
   data = JSON.parse(File.read(file_path))
+  puts "data in #{file_path} is \n#{data}"
   { file_name: File.basename(file_path), data: data }
 end
 
